bda8f704c658a8958e075c4776ba84950c03e7ed
victormigueltejeda <victormigueltejeda@gmail.com>
2022-04-29 17:05:23 -0400

    primer commit de dogGram

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+  output:{
+    filename:"app.bundle.js"
+  },
+  module:{
+    rules:[{
+      test:/\.js$/,
+      exclude:"/node_modules",
+      use:{
+        loader:"babel-loader",
+        options:{
+          presets:[
+            "@babel/preset-env",
+            "@babel/preset-react"
+          ]
+        }
+      }
+    }]
+  },
+  mode:"development",
+  plugins:[
+    new HtmlWebpackPlugin({
+      template:"./src/index.html"
+    })
+  ]
+}
